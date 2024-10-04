792eab59ca0a7331cf6c4139fa55fa1085b24329
Ilya Berkov <berkov.ilya93@gmail.com>
2024-10-04 22:08:10 +0300

    задача 3.4 и 3.5 решены

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,8 @@
 function showSalary(users, age) {
-  // ваш код...
+  let str = '';
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].age <= age && i < users.length) {
+      str += (users[i].name + ", " + users[i].balance+ "\n") }
+  }
+  return str.trim();
 }
