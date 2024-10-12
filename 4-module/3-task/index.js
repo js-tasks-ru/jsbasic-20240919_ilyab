346180219a916a75e26fa73e5b080d2a200d8bee
Ilya Berkov <berkov.ilya93@gmail.com>
2024-10-12 08:56:01 +0300

    задача 4.3 решена

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,26 @@
 function highlight(table) {
-  // ваш код...
+
+  for (let i = 0; i < table.rows.length; i++) {
+    let row = table.rows[i];
+
+    if (row.cells[3].dataset.available === "true") {
+      row.classList.add("available");
+    } else if (row.cells[3].dataset.available === "false"){
+      row.classList.add("unavailable");
+    }
+
+    if (!row.cells[3].dataset.available) {
+      row.setAttribute("hidden", true);
+    }
+
+    if (row.cells[2].textContent === "m") {
+      row.classList.add("male");
+    } else if (row.cells[2].textContent === "f"){
+      row.classList.add("female");
+    }
+
+    if (parseInt(row.cells[1].textContent) < 18) {
+      row.style.textDecoration = "line-through";
+    }
+  }
 }
